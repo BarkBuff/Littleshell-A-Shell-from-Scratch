d23ccd494f722beb21ac1ee850fa9f7097903846
codemaestro <silveryfreakz@gmail.com>
2025-10-19 11:01:42 +0530

    Implement the readline() function to read characters

--- sandbox.c
+++ sandbox.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define LSH_RL_BUFSIZE 1024
 
 void lsh_loop(void);
+char *lsh_read_line(void);
 
 int main(int argc, char **argv) {
     lsh_loop();
@@ -9,17 +13,51 @@ int main(int argc, char **argv) {
 
 void lsh_loop(void)
 {
-  char *line;
-  char **args;
-  int status;
+    char *line;
+    char **args;
+    int status;
 
-  do {
-    printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
+    do {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
 
-    free(line);
-    free(args);
-  } while (status);
+        free(line);
+        free(args);
+    } while (status);
+}
+
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        c = getchar();
+
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 }
